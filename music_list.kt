a2c8d744458041b373f97cda896e578eb08d2dec
Carlos Garcia <stark.jc.garcia@gmail.com>
2024-02-12 16:17:30 -0600

    music file added - version 1.4

---
+++ music_list.kt
@@ -0,0 +1,13 @@
+
+data class Song(
+    val name: String,
+    val interpreter: String,
+    val album: String,
+    val year: Int
+)
+
+val librarySongs = listOf(
+    Song("El alma al aire", "Alejandro Sanz", "El alma al aire", 2000),
+    Song("A la primera persona", "Alejandro Sanz", "El tren de los momentos", 2008),
+    
+)
