f5211c7f493285cfb91e1617cfb046559ca970ff
Carlos Garcia <stark.jc.garcia@gmail.com>
2024-02-13 17:55:28 -0600

    Start Mouredev Tutorial - version 1.8

--- music_list.kt
+++ music_list.kt
@@ -9,5 +9,6 @@ data class Song(
 val librarySongs = listOf(
     Song("El alma al aire", "Alejandro Sanz", "El alma al aire", 2000),
     Song("A la primera persona", "Alejandro Sanz", "El tren de los momentos", 2008),
+    Song("Si hay Dios", "Alejandro Sanz", "Más", 1997),
     
 )
