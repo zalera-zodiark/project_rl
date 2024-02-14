28ee913982d07e18d3177986b243dd2c24f4601e
Carlos Garcia <stark.jc.garcia@gmail.com>
2024-02-13 19:04:43 -0600

    add new C functions - version 1.8.1

--- util.h
+++ util.h
@@ -14,3 +14,19 @@ int addOne(int n) {
 int restOne(int n) {
     return n--;
 }
+
+int sum(int n1, int n2) {
+    return n1 + n2;
+}
+
+int rest(int n1, int n2) {
+    return n1 - n2;
+}
+
+int multiplication(int n1, int n2) {
+    return n1 * n2;
+}
+
+int division(int n1, int n2) {
+    return n1 / n2;
+}
