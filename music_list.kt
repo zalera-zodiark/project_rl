d6fe73a3d6cf96d7a2586e5d55aa4ab6be80022b
Carlos Garcia <stark.jc.garcia@gmail.com>
2024-02-15 19:10:32 -0600

    music function add - version 1.9.2

--- music_list.kt
+++ music_list.kt
@@ -10,5 +10,10 @@ val librarySongs = listOf(
     Song("El alma al aire", "Alejandro Sanz", "El alma al aire", 2000),
     Song("A la primera persona", "Alejandro Sanz", "El tren de los momentos", 2008),
     Song("Si hay Dios", "Alejandro Sanz", "Más", 1997),
-    
 )
+
+fun printList() {
+    librarySongs.forEach {
+        print(it)
+    }
+}
