d586d2fefdb1e6cbbb435ccd0463bb6877225884
i3-Charly <darkness.wolfzodiark@gmail.com>
2024-02-15 19:01:59 -0600

    add user login C code - version 1.9.1

--- codigo_c.c
+++ codigo_c.c
@@ -3,5 +3,12 @@
 int main() {
     printf("Hola desde programación en C!!!");
 
+    char nombre[20];
+
+    printf("Dame tu nombre buen programador: ");
+    scanf("%s", &nombre);
+
+    printf("Bienvenido maestro %s", nombre);
+
     return 0;
 }
